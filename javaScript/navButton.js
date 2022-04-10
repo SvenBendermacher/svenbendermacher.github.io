17129da35b5a9a8972f4845ffe5da1e52d2a8881
SvenBendermacher <40065793+SvenBendermacher@users.noreply.github.com>
2022-04-10 20:03:16 +0200

    Add files via upload

---
+++ javaScript/navButton.js
@@ -0,0 +1,9 @@
+
+
+function openNav() {
+  document.getElementsByClassName("sidenav")[0].style.width = "300px";
+}
+
+function closeNav() {
+  document.getElementsByClassName("sidenav")[0].style.width = "0";
+}
