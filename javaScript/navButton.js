c69593209a570b60f79371fa2b4376baca99c95e
SvenBendermacher <40065793+SvenBendermacher@users.noreply.github.com>
2022-04-11 12:38:27 +0200

    Delete javaScript directory

--- javaScript/navButton.js
+++
@@ -1,9 +0,0 @@
-
-
-function openNav() {
-  document.getElementsByClassName("sidenav")[0].style.width = "300px";
-}
-
-function closeNav() {
-  document.getElementsByClassName("sidenav")[0].style.width = "0";
-}
