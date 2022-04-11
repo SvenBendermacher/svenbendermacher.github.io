46c5e1c11bfec7acbf80a2220826d565eb8f4f3d
SvenBendermacher <40065793+SvenBendermacher@users.noreply.github.com>
2022-04-11 12:39:31 +0200

    Add files via upload

---
+++ javaScript/timeline.js
@@ -0,0 +1,16 @@
+
+  window.sr = ScrollReveal();
+
+  sr.reveal('.js--fadeInLeft', {
+	    origin: 'left',
+	    distance: '300px',
+		  easing: 'ease-in-out',
+	    duration: 800,
+	  });
+
+	  sr.reveal('.js--fadeInRight', {
+	    origin: 'right',
+	    distance: '300px',
+	    easing: 'ease-in-out',
+	    duration: 800,
+	  });
